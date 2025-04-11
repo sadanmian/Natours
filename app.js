1d044b87b88d2a8d805188210d0f09cbe0cd0bd2
Sadan Mian <sadanmiyan007@gmail.com>
2025-04-11 16:41:31 +0530

    Serving Static Files

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const app = express();
 // 1) Middlewares
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log('Hello from the middleware');
