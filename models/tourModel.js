624c36e1938d62542a96fb255b148621dd6a64bd
Sadan Mian <sadanmiyan007@gmail.com>
2025-04-21 18:25:29 +0530

    Data Validation: Built-In Validators

--- models/tourModel.js
+++ models/tourModel.js
@@ -8,6 +8,8 @@ const toursSchema = new mongoose.Schema(
       required: [true, 'A tour must have a name'],
       unique: true,
       trim: true,
+      maxlength: [40, 'A tour name must have less or equal to 40 characters.'],
+      minlength: [10, 'A tour name must have more or equal to 10 characters.'],
     },
     slug: String,
     duration: {
@@ -21,10 +23,16 @@ const toursSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       require: [true, 'A tour must have a difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either: easy, medium, difficult',
+      },
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0'],
     },
     ratingsQuantity: {
       type: Number,
