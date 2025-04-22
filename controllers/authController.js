a3ad741276a4d49e6d895c67da9a7a2098832bb1
Sadan Mian <sadanmiyan007@gmail.com>
2025-04-22 23:42:20 +0530

    Protecting Tour Routes - Part 1

--- controllers/authController.js
+++ controllers/authController.js
@@ -68,3 +68,36 @@ exports.login = async (req, res, next) => {
     });
   }
 };
+
+exports.protect = async (req, res, next) => {
+  try {
+    // 1) Getting token and check if it's there
+    let token;
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith('Bearer')
+    ) {
+      token = req.headers.authorization.split(' ')[1];
+    }
+    console.log(token);
+
+    if (!token) {
+      return res.status(401).json({
+        status: 'fail',
+        message: 'You are not logged in! Please log in to get access.',
+      });
+    }
+    // 2) Verification token
+
+    // 3) Check if user still exits
+
+    // 4) Check if user changed password after the token was issued
+
+    next();
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      message: error,
+    });
+  }
+};
