466385e0eb77a3ff94bf64ddcfcb9b7c2e6c9c46
Sadan Mian <sadanmiyan007@gmail.com>
2025-04-23 14:35:35 +0530

    Sending Emails with Nodemailer

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const User = require('./../models/userModel');
 const jwt = require('jsonwebtoken');
 const { promisify } = require('util');
+const sendEmail = require('./../utils/email');
 
 const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -130,23 +131,42 @@ exports.restrictTo = (...roles) => {
 };
 
 exports.forgotPassword = async (req, res, next) => {
-  try {
-    // 1) Get user based on posted email
-    const user = await User.findOne({ email: req.body.email });
-    if (!user) {
-      return res.status(404).json({
-        status: 'fail',
-        message: 'There is no user with this email address',
-      });
-    }
-    // 2) Generate the random token
-    const resetToken = user.createPasswordResetToken();
-    await user.save({ validateBeforeSave: false });
-    // 3) Sent it to user's email
-  } catch (error) {
-    res.status(400).json({
+  // 1) Get user based on posted email
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    return res.status(404).json({
       status: 'fail',
-      message: error,
+      message: 'There is no user with this email address',
+    });
+  }
+  // 2) Generate the random token
+  const resetToken = user.createPasswordResetToken();
+  await user.save({ validateBeforeSave: false });
+  // 3) Sent it to user's email
+  const resetURL = `${req.protocol}://${req.get(
+    'host'
+  )}/api/v1/users/resetPassword/${resetToken}`;
+
+  const message = `Forget your passowrd? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\nIf you didn't forget your password, please ignore this email!`;
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: 'Your password reset token (valid for 10 min)',
+      message,
+    });
+
+    res.status(200).json({
+      status: 'success',
+      message: 'Token send to email!',
+    });
+  } catch (error) {
+    user.createPasswordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+    await user.save({ validateBeforeSave: false });
+
+    return res.status(500).json({
+      status: 'fail',
+      message: 'There was an error sending the email. Try again later!' + error,
     });
   }
 };
---
+++ utils/email.js
@@ -0,0 +1,24 @@
+const nodeMailer = require('nodemailer');
+
+const sendEmail = async (options) => {
+  // 1) Create a transporter
+  const tranporter = nodeMailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      password: process.env.EMAIL_PASSWORD,
+    },
+  });
+  // 2) Define the email options
+  const mailOptions = {
+    from: 'Sadan Mian <hello@sadanmian.com>',
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+  };
+  // 3) Actually send the email
+  await tranporter.sendMail(mailOptions);
+};
+
+module.exports = sendEmail;
